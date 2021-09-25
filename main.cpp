f9b2043d50c284e8997a4ebf71e8e213e8c0bee0
OKukotin <oleksandr.kukotin@gmail.com>
2021-09-25 16:48:44 +0300

    The main parameters and character selection function are created

--- main.cpp
+++ main.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
 #include <string>
 
-using namespace std;
+#define TEXT_SIZE 100
+
+char getEncryptedSymbol(char symbol, int shift){
+    for (char i = 'a'; i <= 'z'; i++) {
+        std::cout << i << '\n';
+    }
+}
 
 int main(int argc, char const *argv[]) {
-    /* code */
+    string input;
+    int shift;
+    bool method;
+    
+    std::cout << "Choice the operation (1 - encrypt, 0 - decrypt)" << '\n';
+    std::cin >> method;
+
+    std::cout << "Enter the size of shift (positive integer): " << '\n';
+    std::cin >> shift;
+
+    std::cout << "Enter the text you want to encrypt (on English): ";
+    std::cin.getline(input, TEXT_SIZE);
+
+    for (int i = 0; i < input.size(); i++) {
+        if (method) {
+            getEncryptedSymbol()
+        }
+    }
+    std::cout << std::endl << "Encypted text: " << input << '\n';
     return 0;
 }
