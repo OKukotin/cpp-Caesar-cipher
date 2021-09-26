4687edb57d6a60366645c2b1bd97b41c5c6517db
OKukotin <oleksandr.kukotin@gmail.com>
2021-09-26 23:34:25 +0300

    Completed verifications of input values. Need to complete: TODO

--- main.cpp
+++ main.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 #include <string>
+#include <cctype>
 
-#define a int('a')
-#define A int('A')
-
-char getEncryptedASCIISymbol(char symbol, const int shift) {
-    int symbolCode = (int(symbol) + shift);
-    // TODO: add a mechanism of non-going beyond the characters of the alphabet
-    if (symbolCode < a || symbolCode > a + 25) {
-        /* code */
+unsigned char getEncryptedASCIISymbol(unsigned char symbol, const int shift) {
+    int symbolCode = int(symbol) + shift;
+    // TODO: add a mechanism of non-going beyond the printable characters of ASCII
+    while(!isprint(symbolCode)) {
+        std::cout << "loop" << '\n';
     }
     return char(symbolCode);
 }
@@ -18,11 +16,20 @@ int main(int argc, char const *argv[]) {
     int shift;
     bool method;
 
-    std::cout << "Enter the size of shift (positive integer): ";
-    std::cin >> shift;
+    while (std::cout << "Enter the size of shift (positive integer): " && !(std::cin >> shift)) {
+        std::cin.clear();
+        std::cin.ignore();
+        std::cout << "Please, enter a number according to the instruction!\n";
+    }
+    if(shift < 0) {
+        shift = -shift;
+    }
 
-    std::cout << "Choice the operation (1 - encrypt, 0 - decrypt): ";
-    std::cin >> method;
+    while (std::cout << "Choice the operation (1 - encrypt, 0 - decrypt): " && !(std::cin >> method)) {
+        std::cin.clear();
+        std::cin.ignore();
+        std::cout << "Please, enter a digit according to the instruction!\n";
+    }
     shift = method ? shift : -shift;
 
     std::cout << "Enter the text: ";
@@ -31,9 +38,7 @@ int main(int argc, char const *argv[]) {
     const int inputSize = input.size();
 
     for (int i = 0; i < inputSize; i++) {
-        const int current = int(input[i]);
-        if (current >= a && current <= a + 25
-            || current >= A && current <= A + 25) {
+        if (isalpha(int(input[i]))) {
             input[i] = getEncryptedASCIISymbol(input[i], shift);
         }
         else {
